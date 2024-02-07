92c548bdcaf48bbc0a423dd599fbb0925e1bc924
Norman Torres <norman.torres.mx@gmail.com>
2024-02-07 16:23:41 -0600

    pagina cv en github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://norman404.github.io',
-  base: '/minimalist-portfolio-json',
+  site: 'https://cv.molcajete.dev',
 });
