76ebf02e023f13888112bfc782ca34043dac40e1
Norman Torres <norman.torres.mx@gmail.com>
2024-02-13 15:47:02 -0600

    Add soport i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   site: 'https://cv.molcajete.dev',
 });
